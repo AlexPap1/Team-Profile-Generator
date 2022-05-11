0dc8c036af39f0ffadd3461758a630323e1c03e8
AlexPap <arpappagallo@gmail.com>
2022-05-11 10:47:01 -0400

    made a lot of formatting changes, adjust challenge 9 template to challenge 10. Added files

--- index.js
+++ index.js
@@ -3,20 +3,48 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-// Array of questions for user input:
-const questions = [
-
-    {
-        type:'input',
-        name:'username',
-        message:'What is your GitHub Username?',
-    },
-    {
-        type:'input',
-        name:'email',
-        message:'What is your email address?',
-    },
-];
+// Create a function to initialize app
+function init() {
+    // inquirer.prompt(questions)
+    //     .then(function (answers) {
+    //         writeToFile("./dist/index.html", generateMarkdown(answers))
+    //     })
+    function manager(){
+        inquirer.prompt([
+        {
+            type:'input',
+            name:'username',
+            message:'What is your GitHub Username?',
+        },
+        {
+            type:'input',
+            name:'email',
+            message:'What is your email address?',
+        },
+        {
+            type:'input',
+            name:'manager',
+            message:'What is the name of the team manager?',
+        },
+        {
+            type:'input',
+            name:'managerId',
+            message:'What is the ID of the team manager?',
+        },
+        {
+            type:'input',
+            name:'managerEmail',
+            message:'What is the email address of the team manager?',
+        },
+        {
+            type:'input',
+            name:'officeNumber',
+            message:'What is the office number?',
+        },
+        ]);
+    }
+    manager();
+};
 
 //Create a function to write README file
 function writeToFile(fileName, data) {
@@ -24,14 +52,6 @@ function writeToFile(fileName, data) {
         //console logs error or success
         err ? console.log(err) : console.log('Success!')
     )
-};
-
-// Create a function to initialize app
-function init() {
-    inquirer.prompt(questions)
-        .then(function (answers) {
-            writeToFile("./dist/index.html", generateMarkdown(answers))
-        })
 };
 
 // Function call to initialize app
